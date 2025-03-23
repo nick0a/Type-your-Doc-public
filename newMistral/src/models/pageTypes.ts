a3c6ca80372142e2058fdcc1999ce2ef46919922
Nicholas Clarke <nick@grey-wing.com>
2025-03-23 23:40:00 +0800

    Refactor pageTypes.ts with improved documentation and structure

---
+++ newMistral/src/models/pageTypes.ts
@@ -0,0 +1,116 @@
+/**
+ * pageTypes.ts
+ * Contains types and models for document page classification
+ */
+
+/**
+ * Main document categories
+ */
+export enum MainDocumentCategory {
+  MASTERS_CARGO_DOCS = 'MASTERS_CARGO_DOCS',
+  AGENTS_SOF = 'AGENTS_SOF',
+  CHARTER_PARTY_DOCS = 'CHARTER_PARTY_DOCS'
+}
+
+/**
+ * Subcategories for MASTERS_CARGO_DOCS
+ */
+export enum MastersCargoDocType {
+  CARGO_DOCUMENTS_TOC = 'CARGO_DOCUMENTS_TOC',
+  STATEMENT_OF_FACTS_FIRST = 'STATEMENT_OF_FACTS_FIRST',
+  STATEMENT_OF_FACTS_ADDITIONAL = 'STATEMENT_OF_FACTS_ADDITIONAL',
+  NOTICE_OF_READINESS_FIRST = 'NOTICE_OF_READINESS_FIRST',
+  NOTICE_OF_READINESS_RETENDERED = 'NOTICE_OF_READINESS_RETENDERED',
+  LETTER_OF_PROTEST_DELAYS = 'LETTER_OF_PROTEST_DELAYS',
+  LETTER_OF_PROTEST_REFUSAL = 'LETTER_OF_PROTEST_REFUSAL',
+  LETTER_OF_PROTEST_SLOW_LOADING = 'LETTER_OF_PROTEST_SLOW_LOADING',
+  LETTER_OF_PROTEST_SLOW_DISCHARGING = 'LETTER_OF_PROTEST_SLOW_DISCHARGING',
+  LETTER_OF_PROTEST_FREE_PRATIQUE = 'LETTER_OF_PROTEST_FREE_PRATIQUE',
+  ULLAGE_REPORT_FIRST = 'ULLAGE_REPORT_FIRST',
+  ULLAGE_REPORT_ADDITIONAL = 'ULLAGE_REPORT_ADDITIONAL',
+  EMPTY_TANK_CERTIFICATE = 'EMPTY_TANK_CERTIFICATE',
+  PUMPING_LOG_FIRST = 'PUMPING_LOG_FIRST',
+  PUMPING_LOG_ADDITIONAL = 'PUMPING_LOG_ADDITIONAL',
+  AUTHORISATION_BILLS_OF_LADING = 'AUTHORISATION_BILLS_OF_LADING',
+  TANK_CLEANLINESS_CERTIFICATE = 'TANK_CLEANLINESS_CERTIFICATE',
+  LETTER_OF_PROTEST_BERTHING = 'LETTER_OF_PROTEST_BERTHING',
+  LETTER_OF_PROTEST_GENERAL = 'LETTER_OF_PROTEST_GENERAL'
+}
+
+/**
+ * Subcategories for AGENTS_SOF
+ */
+export enum AgentsSofType {
+  SHIPPING_ORDER = 'SHIPPING_ORDER',
+  CARGO_MANIFEST = 'CARGO_MANIFEST',
+  CONFIRMATION_CHANDLERY_SUPPLY = 'CONFIRMATION_CHANDLERY_SUPPLY',
+  STATEMENT_OF_FACTS_FIRST = 'STATEMENT_OF_FACTS_FIRST',
+  STATEMENT_OF_FACTS_ADDITIONAL = 'STATEMENT_OF_FACTS_ADDITIONAL'
+}
+
+/**
+ * Subcategories for CHARTER_PARTY_DOCS
+ */
+export enum CharterPartyDocType {
+  CHARTER_PARTY = 'CHARTER_PARTY',
+  MAIN_TERMS = 'MAIN_TERMS',
+  RECAP_NOTE = 'RECAP_NOTE',
+  VOYAGE_ORDER = 'VOYAGE_ORDER',
+  RIDER_CLAUSES = 'RIDER_CLAUSES',
+  WARRANTY = 'WARRANTY',
+  ADDENDUM = 'ADDENDUM',
+  SUPPLEMENTARY_TERMS = 'SUPPLEMENTARY_TERMS',
+  NARROWED_LAYCAN = 'NARROWED_LAYCAN',
+  OTHER = 'OTHER'
+}
+
+/**
+ * Union type for all document types
+ */
+export type DocumentType = 
+  | MastersCargoDocType
+  | AgentsSofType
+  | CharterPartyDocType;
+
+/**
+ * Classification result for a single page
+ */
+export interface PageClassification {
+  pageNumber: number;
+  mainCategory: MainDocumentCategory;
+  documentType: DocumentType;
+  confidence: number;
+  portNames: string[];
+}
+
+/**
+ * Classification result for an entire document
+ */
+export interface DocumentClassification {
+  documentName: string;
+  totalPages: number;
+  ports: string[];
+  pages: PageClassification[];
+}
+
+/**
+ * OCR processing result
+ */
+export interface OCRResult {
+  text: string;
+  confidence?: number;
+}
+
+/**
+ * Custom error for document classification problems
+ */
+export class DocumentClassificationError extends Error {
+  constructor(
+    message: string,
+    public pageData?: any,
+    public classification?: any
+  ) {
+    super(message);
+    this.name = 'DocumentClassificationError';
+  }
+} 
